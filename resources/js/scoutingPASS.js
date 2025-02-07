09ed25ea14bada6a74d63ca4733e90b77ae453d2
Rockefeller8873 <Rockefeller8873@outlook.com>
2025-02-07 08:33:16 -0500

    Update scoutingPASS.js

--- resources/js/scoutingPASS.js
+++ resources/js/scoutingPASS.js
@@ -1,3 +1,4 @@
+
 // ScoutingPASS.js
 //
 // The guts of the ScountingPASS application
@@ -11,8 +12,9 @@ var initialX = null;
 var xThreshold = 0.3;
 var slide = 0;
 var enableGoogleSheets = false;
-var pitScouting = true;
+var pitScouting = false;
 var checkboxAs = 'YN';
+var ColWidth = '200px';
 
 // Options
 var options = {
@@ -21,9 +23,8 @@ var options = {
   quietZoneColor: '#FFFFFF'
 };
 
-// Must be filled in: e=event, m=match#, l=level(q,qf,sf,f), t=team#, r=robot(r1,r2,b1..), s=scouter
-//var requiredFields = ["e", "m", "l", "t", "r", "s", "as"];
-var requiredFields = ["e", "m", "r", "l" "t" "s", "as"];
+// Built from the JSON
+var requiredFields = []; //["e", "m", "l", "r", "s", "as"];
 
 function addTimer(table, idx, name, data) {
   var row = table.insertRow(idx);
@@ -151,12 +152,14 @@ function addTimer(table, idx, name, data) {
 function addCounter(table, idx, name, data) {
   var row = table.insertRow(idx);
   var cell1 = row.insertCell(0);
+  cell1.style.width = ColWidth;
   cell1.classList.add("title");
   if (!data.hasOwnProperty('code')) {
     cell1.innerHTML = `Error: No code specified for ${name}`;
     return idx + 1;
   }
   var cell2 = row.insertCell(1);
+  cell2.style.width = ColWidth;
   cell1.innerHTML = name + '&nbsp;';
   if (data.hasOwnProperty('tooltip')) {
     cell1.setAttribute("title", data.tooltip);
@@ -331,8 +334,7 @@ function addClickableImage(table, idx, name, data) {
   inp.setAttribute("value", "none");
   if (data.hasOwnProperty('allowableResponses')) {
     let responses = data.allowableResponses.split(' ').map(Number)
-    console.log(responses)
-      inp.setAttribute("value", responses);
+    inp.setAttribute("value", responses);
   }
   cell.appendChild(inp);
 
@@ -402,12 +404,14 @@ function addClickableImage(table, idx, name, data) {
 function addText(table, idx, name, data) {
   var row = table.insertRow(idx);
   var cell1 = row.insertCell(0);
+  cell1.style.width = ColWidth;
   cell1.classList.add("title");
   if (!data.hasOwnProperty('code')) {
     cell1.innerHTML = `Error: No code specified for ${name}`;
     return idx + 1;
   }
   var cell2 = row.insertCell(1);
+  cell2.style.width = ColWidth;
   cell1.innerHTML = name + '&nbsp;';
   if (data.hasOwnProperty('tooltip')) {
     cell1.setAttribute("title", data.tooltip);
@@ -455,12 +459,14 @@ function addText(table, idx, name, data) {
 function addNumber(table, idx, name, data) {
   var row = table.insertRow(idx);
   var cell1 = row.insertCell(0);
+  cell1.style.width = ColWidth;
   cell1.classList.add("title");
   if (!data.hasOwnProperty('code')) {
     cell1.innerHTML = `Error: No code specified for ${name}`;
     return idx + 1;
   }
   var cell2 = row.insertCell(1);
+  cell2.style.width = ColWidth;
   cell1.innerHTML = name + '&nbsp;';
   if (data.hasOwnProperty('tooltip')) {
     cell1.setAttribute("title", data.tooltip);
@@ -518,12 +524,14 @@ function addNumber(table, idx, name, data) {
 function addRadio(table, idx, name, data) {
   var row = table.insertRow(idx);
   var cell1 = row.insertCell(0);
+  cell1.style.width = ColWidth;
   cell1.classList.add("title");
   if (!data.hasOwnProperty('code')) {
     cell1.innerHTML = `Error: No code specified for ${name}`;
     return idx + 1;
   }
   var cell2 = row.insertCell(1);
+  cell2.style.width = ColWidth;
   cell1.innerHTML = name + '&nbsp;';
   if (data.hasOwnProperty('tooltip')) {
     cell1.setAttribute("title", data.tooltip);
@@ -577,6 +585,7 @@ function addRadio(table, idx, name, data) {
 function addCheckbox(table, idx, name, data) {
   var row = table.insertRow(idx);
   var cell1 = row.insertCell(0);
+  cell1.style.width = ColWidth;
   cell1.classList.add("title");
   if (!data.hasOwnProperty('code')) {
     cell1.innerHTML = `Error: No code specified for ${name}`;
@@ -584,6 +593,7 @@ function addCheckbox(table, idx, name, data) {
   }
   var cell2 = row.insertCell(1);
   cell1.innerHTML = name + '&nbsp;';
+  cell2.style.width = ColWidth;
   if (data.hasOwnProperty('tooltip')) {
     cell1.setAttribute("title", data.tooltip);
   }
@@ -665,6 +675,12 @@ function addElement(table, idx, data) {
   return idx
 }
 
+function buildRequiredElementList(element) {
+	if (element.required == "true") {
+		requiredFields.push(element.code);
+	}
+}
+
 function configure() {
   try {
     var mydata = JSON.parse(config_data);
@@ -675,6 +691,7 @@ function configure() {
     var table = document.getElementById("prematch_table")
     var row = table.insertRow(0);
     var cell1 = row.insertCell(0);
+	cell1.style.width = ColWidth;
     cell1.innerHTML = `Error parsing configuration file: ${err.message}<br><br>Use a tool like <a href="http://jsonlint.com/">http://jsonlint.com/</a> to help you debug your config file`
     return -1
   }
@@ -725,6 +742,7 @@ function configure() {
   var idx = 0;
   pmc.forEach(element => {
     idx = addElement(pmt, idx, element);
+	buildRequiredElementList(element);
   });
 
   // Configure auton screen
@@ -786,16 +804,16 @@ function validateData() {
   var errStr = "";
   for (rf of requiredFields) {
     var thisRF = document.forms.scoutingForm[rf];
-    if (thisRF.value == "[]" || thisRF.value.length == 0) {
-      if (rf == "as") {
-        rftitle = "Auto Start Position"
-      } else {
-        thisInputEl = thisRF instanceof RadioNodeList ? thisRF[0] : thisRF;
-        rftitle = thisInputEl.parentElement.parentElement.children[0].innerHTML.replace("&nbsp;","");
-      }
-      errStr += rf + ": " + rftitle + "\n";
-      ret = false;
-    }
+	if (thisRF.value == "[]" || thisRF.value.length == 0) {
+	  if (rf == "as") {
+		rftitle = "Auto Start Position"
+	  } else {
+		thisInputEl = thisRF instanceof RadioNodeList ? thisRF[0] : thisRF;
+		rftitle = thisInputEl.parentElement.parentElement.children[0].innerHTML.replace("&nbsp;","");
+	  }
+	  errStr += rf + ": " + rftitle + "\n";
+	  ret = false;
+	}
   }
   if (ret == false) {
     alert("Enter all required values\n" + errStr);
@@ -897,7 +915,7 @@ function qr_clear() {
   qr.clear()
 }
 
-function clearForm(id=clearForm) {
+function clearForm() {
   var match = 0;
   var e = 0;
 
@@ -1039,8 +1057,19 @@ function drawFields(name) {
     var shape = document.getElementById("shape_" + code);
     let shapeArr = shape.value.split(' ');
     var ctx = f.getContext("2d");
-    ctx.clearRect(0, 0, f.width, f.height);
-    ctx.drawImage(img, 0, 0, f.width, f.height);
+    var imgWidth = img.width;
+    var imgHeight = img.height;
+    let scale_factor = Math.min(ctx.canvas.width / img.width, ctx.canvas.height / img.height);
+    let newWidth = img.width * scale_factor;
+    let newHeight = img.height * scale_factor;
+    if (newWidth > 0) {
+      ctx.canvas.width = newWidth
+    }
+    if (newHeight > 0) {
+      ctx.canvas.height = newHeight
+    }
+    ctx.clearRect(0, 0, newWidth, newHeight);
+    ctx.drawImage(img, 0, 0, newWidth, newHeight);
 
     var xyStr = document.getElementById("XY_" + code).value
     if (JSON.stringify(xyStr).length > 2) {
@@ -1384,7 +1413,6 @@ function copyData(){
   navigator.clipboard.writeText(getData(dataFormat));
   document.getElementById('copyButton').setAttribute('value','Copied');
 }
-document.getElementById("clearForm").addEventListener("click", clearForm);
 
 window.onload = function () {
   let ret = configure();
